e89cc3e844687aa5accd02783bbe3cdb0995e13d
fgunscot <fgunscot@gmail.com>
2022-04-19 23:42:52 +0100

    added env config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
+const debug = process.env.NODE_ENV !== "production";
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['picsum.photos'],
   },
+  assetPrefix: !debug ? 'https://fgunscot.github.io/cv-viewer/' : '',
 }
 
 module.exports = nextConfig
