34d161c1819605af20d30c35e59d2476b60be2cd
fgunscot <100605885+fgunscot@users.noreply.github.com>
2022-04-19 21:23:15 +0100

    Delete out directory

--- out/_next/static/Ed2xgOrOT2w5vFOSeGGDM/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/710-8d1d59246a4f39d8.js","static/css/77dc5d81fde0279c.css","static/chunks/pages/index-5d90d427dc880186.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
