ae615f10e26549a7fc3ad5d5269b8efd432b2345
fgunscot <fgunscot@gmail.com>
2022-05-04 14:02:13 +0100

    Deploy gh-pages

---
+++ out/_next/static/7JDQNMUIawilO4KRGdUUf/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/710-a6c090b9ed70c7af.js","static/css/77dc5d81fde0279c.css","static/chunks/pages/index-e458b882037123f4.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
